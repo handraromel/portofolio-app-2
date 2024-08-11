2325cf34c0ade145477d8daf83a54d1d7eb9b4bc
handraromel <handromel@gmail.com>
2024-08-11 12:55:19 +0700

    updated routing

--- react-frontend/src/routes/AppRouter.tsx
+++ react-frontend/src/routes/AppRouter.tsx
@@ -39,7 +39,7 @@ const AppRouter: React.FC = () => {
   const isAuthenticated = useAppSelector((state) => state.auth.isAuthenticated);
 
   return (
-    <Router basename="/savoryscript">
+    <Router>
       <Suspense
         fallback={
           <div className="flex min-h-screen items-center justify-center bg-gray-50">
