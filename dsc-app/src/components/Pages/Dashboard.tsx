d43cf4284a4bac11b100de683745ef594c458f84
handraromel <handromel@gmail.com>
2025-02-02 12:24:38 +0700

    refactor: update welcome message to reflect new branding for Daily Sales Control

--- dsc-app/src/components/Pages/Dashboard.tsx
+++ dsc-app/src/components/Pages/Dashboard.tsx
@@ -7,7 +7,9 @@ const Dashboard: React.FC = () => {
   return (
     <Card fullHeight>
       <div className="flex h-full flex-col items-center justify-center">
-        <h1 className="mb-4 text-4xl font-bold">Welcome to Savory Script</h1>
+        <h1 className="mb-4 text-4xl font-bold">
+          Welcome to Cheppy 182 - Daily Sales Control{" "}
+        </h1>
         <h2 className="mb-4 text-xl">Our awesome journey starts here.</h2>
         {currentUser && (
           <div className="text-center">
